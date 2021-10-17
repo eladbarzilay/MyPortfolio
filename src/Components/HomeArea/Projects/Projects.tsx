ad94e9dc0db6d9903154571e7df239e4b15f362a
eladbarzilay <eladbarzilay53@gmail.com>
2021-10-17 13:04:45 +0300

    ע

--- src/Components/HomeArea/Projects/Projects.tsx
+++ src/Components/HomeArea/Projects/Projects.tsx
@@ -5,7 +5,7 @@ function Projects(): JSX.Element {
         <div className="Projects">
 			<a href="https://github.com/eladbarzilay/couponSystemReact" target="#"> coupon system react</a>
         <br />
-             {/* <a href="https://github.com/eladbarzilay/CouponSystemV2Spring-" target="#"> coupon system spring</a> */}
+             <a href="https://github.com/eladbarzilay/CouponSystemV2Spring-" target="#"> coupon system spring</a>
         </div>
     );
 }
